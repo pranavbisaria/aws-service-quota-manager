cd0c12f9ba1524278f2a9399f85e9fbb184a58e8
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-06-29 00:22:48 +0530

    Initial commit

---
+++ test/aws-quota-manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsQuotaManager from '../lib/aws-quota-manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-quota-manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsQuotaManager.AwsQuotaManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
